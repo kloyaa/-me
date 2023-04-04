1fb4d6cceaa46e055259a133613fa63ade49a7fa
Kolya Madridano <kolya.madridano@gmail.com>
2023-04-04 16:46:23 +0800

    Fix/Main.page add description

--- src/page/main.page.jsx
+++ src/page/main.page.jsx
@@ -1,10 +1,11 @@
-import { Heading } from '@chakra-ui/react'
+import { Box, Heading } from '@chakra-ui/react'
 import React from 'react'
 import { Navbar } from '../component/navbar.component'
 
 function Main() {
   return <>
     <Heading>This is a main page</Heading>
+    <Box>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, exercitationem deserunt nisi nulla pariatur possimus, adipisci mollitia tempore animi nesciunt ipsa repudiandae dolorem at, consequatur corporis accusantium accusamus officia?</Box>
   </>
 }
 
